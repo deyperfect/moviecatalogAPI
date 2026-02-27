76b8d0b74a5511858f688d4f76e0b8d32e02ce37
deyperfect <edrylpalinis@gmail.com>
2026-02-27 09:49:38 +0800

    fix verify middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const router = express.Router()
-    
+const { verifyToken } = require('../middleware/verifyToken')
+
 router.post('/register', userController.registerUser);
-router.get('/details', verify, userController.getUserDetails);
+router.get('/details', verifyToken, userController.getUserDetails);
 router.post('/login', userController.loginUser);
 
 
