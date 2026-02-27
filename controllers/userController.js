4d8dcc89c8f7f90849a1e285074cd64a20a4a55c
deyperfect <edrylpalinis@gmail.com>
2026-02-27 09:45:14 +0800

    Added user details route

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,7 +62,24 @@ const loginUser = async (req, res) => {
     }
 }
 
+const getUserDetails = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error("Error getting user details:", error);
+    return res.status(500).json({ error: "Error getting user details" });
+  }
+};
+
+
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
+  getUserDetails
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/userController');
 const router = express.Router()
     
 router.post('/register', userController.registerUser);
+router.get('/details', verify, userController.getUserDetails);
 router.post('/login', userController.loginUser);
 
 
